1b9724372687d990603899248e70b7b00e6b60c9
samet <142526344+tebberen@users.noreply.github.com>
2025-11-17 03:08:59 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +8,7 @@ export default defineConfig({
     outDir: '../docs/site',
     emptyOutDir: true,
   },
-});
+  define: {
+    global: 'globalThis',
+  }
+})
